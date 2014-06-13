5e49a911b27ff9ee6d46f3bab1c26ff144778fe4
qoire <mr.sun.yao@gmail.com>
2014-06-13 10:36:16 -0400

    first commit

---
+++ desktop/src/com/qoire/walk/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.qoire.walk.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.qoire.walk.walk;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new walk(), config);
+	}
+}
